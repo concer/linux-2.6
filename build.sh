95d679478a343275729ba9ffebe787343f3dba80
Emilio G. Cota <cota@braap.org>
2009-07-10 16:56:10 +0200

    arm/whiterabbit: add build.sh and deploy.sh scripts
    Signed-off-by: Emilio G. Cota <cota@braap.org>

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# build current tree
+# take the config from myconfig
+
+make clean
+make mrproper
+cp myconfig .config
+make ARCH=arm CROSS_COMPILE=/home/wrdev/WR_buildroot/buildroot/build_arm/staging_dir/usr/bin/arm-linux-uclibcgnueabi- $1
+mkimage -A arm -O linux -C none -T kernel -a 20008000 -e 20008000 -n linux-2.6 -d arch/arm/boot/zImage uImage
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# deploy current kernel to White Rabbit
+
+WR_TFTP=/home/wrdev/WR_buildroot/WR_Switch/tftpboot
+
+if test -f $WR_TFTP/uImage
+then
+    cp $WR_TFTP/uImage $WR_TFTP/uImage.backup
+fi
+
+cp uImage $WR_TFTP
