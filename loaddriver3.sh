b6ddaf415de289a386ea7615cb9e8c7172a573ae
Emilio G. Cota <cota@braap.org>
2009-07-19 23:29:39 +0200

    arm/whiterabbit: update init scripts
    Have specific init scripts for each of the two boards.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- loaddriver3.sh
+++ loaddriver3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 insmod wr-mch.ko debug=16
-ifconfig eth1 up 192.168.100.2
+ifconfig eth1 up 192.168.100.3
--- makemodules.sh
+++ makemodules.sh
@@ -3,4 +3,4 @@
 make CONFIG_DEBUG_SECTION_MISMATCH=y ARCH=arm CROSS_COMPILE=/home/wrdev/WR_buildroot/buildroot/build_arm/staging_dir/usr/bin/arm-linux-uclibcgnueabi- $1 modules
 
 scp /home/wrdev/WR_buildroot/WR_Switch/NIC_init_sw/nic_init drivers/net/arm/wr-mch.ko loaddriver.sh root@192.168.1.2:/
-scp /home/wrdev/WR_buildroot/WR_Switch/NIC_init_sw/nic_init drivers/net/arm/wr-mch.ko loaddriver.sh root@192.168.1.3:/
+scp /home/wrdev/WR_buildroot/WR_Switch/NIC_init_sw/nic_init drivers/net/arm/wr-mch.ko loaddriver3.sh root@192.168.1.3:/
