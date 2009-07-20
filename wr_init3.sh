82c2f00fa0529e792bccf9cb76ba155c7d62ef48
Emilio G. Cota <cota@braap.org>
2009-07-20 03:16:39 +0200

    arm/whiterabbit: scripts: make delivery + initialisation automatic
    The only thing we need to run on the boards is wr_init.sh.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

---
+++ wr_init3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+WR_IP=192.168.100.3
+
+bootfpgas.sh
+sleep 1
+cd /
+./nic_init 1>/dev/null &
+sleep 1
+insmod wr-mch.ko debug=16
+sleep 1
+ifconfig eth1 up $WR_IP
+
+echo "FPGA's booted, nic_init running and wr-mch driver installed OK."
+echo "wr-mch running on eth1, ip=$WR_IP"
+
+exit 0
