c84a7996b2fecd5335ee1a69d546f5f289cd35cf
slayer <slayer@kicia2.(none)>
2010-06-10 11:29:30 +0200

    build.sh: use compiler name from the settings' file
    Signed-off-by: Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# build current tree
-# take the config from myconfig
+. ../settings
 
-make clean
-make mrproper
 cp myconfig .config
-make CONFIG_DEBUG_SECTION_MISMATCH=y ARCH=arm CROSS_COMPILE=/home/wrdev/WR_buildroot/buildroot/build_arm/staging_dir/usr/bin/arm-linux-uclibcgnueabi- $1
-mkimage -A arm -O linux -C none -T kernel -a 20008000 -e 20008000 -n linux-2.6 -d arch/arm/boot/zImage uImage
+make CONFIG_DEBUG_SECTION_MISMATCH=y ARCH=arm CROSS_COMPILE=$CC_CPU $1
+$MK_KERNEL_IMAGE -A arm -O linux -C none -T kernel -a 20008000 -e 20008000 -n linux-2.6 -d arch/arm/boot/zImage $KERNEL_IMAGE
